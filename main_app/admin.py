ebabef1f2e35a985b008f6bce51726078293cdb1
User Name <maha.alghuraibi@outlook.com>
2025-11-01 13:39:36 +0300

    update

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Ticket , WorkLog , Reaction 
+from .models import Ticket , WorkLog , Reaction , Profile  
 
 admin.site.register(Ticket) # Ticket
 #-----------------------------------------------------------------------------------------
 admin.site.register(WorkLog) # WorkLog
 #-----------------------------------------------------------------------------------------
 admin.site.register(Reaction) # Reaction
+#-----------------------------------------------------------------------------------------
+admin.site.register(Profile) #Profile
