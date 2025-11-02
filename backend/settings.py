9111e74d6111e84bb517fccf635cc6d3ee2d57f4
User Name <maha.alghuraibi@outlook.com>
2025-11-02 09:36:27 +0300

    update

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os #####
+from datetime import timedelta ####
+from dotenv import load_dotenv ####
+load_dotenv() ####
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96*gd=rs_y_bqy*g82()*3c52rsvw92g1_kz_3v6oh2uyr(f17'
+SECRET_KEY = os.environ.get("SECRET_KEY") #####
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,3 +136,30 @@ CORS_ALLOW_HEADERS = '*'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
 ]
+
+
+# -----------------------------------------------------------------------------------------
+# Django 
+# -----------------------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),   
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Ticket, WorkLog , Reaction , Profile
 from django.contrib.auth import get_user_model ######
-
+from django.contrib.auth.models import User 
 #-----------------------------------------------------------------------------------------
 # Ticket 
 class TicketSerializer(serializers.ModelSerializer):
@@ -36,3 +36,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'profile']
+
+#-----------------------------------------------------------------------------------------
+# user 
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
